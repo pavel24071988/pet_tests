56b6e46b1c37b0022e4f9795afcb6de6a5b20f31
pavel <pavel.sherbakov@vseinstrumenti.ru>
2022-01-26 10:04:57 +0300

    increase tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module pet_tests
 
 go 1.16
 
-require github.com/golang/mock v1.6.0 // indirect
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- tests/max_value_test.go
+++ tests/max_value_test.go
@@ -1,34 +1,83 @@
 package tests
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"pet_tests/pkg/helper"
 	"testing"
 )
 
 func TestMax(t *testing.T) {
 	// Arrange
-	numbers := []int{1, 4, -4, 2, 5, 3, 1}
-	expected := 5
+	testTable := []struct {
+		numbers  []int
+		expected int
+	}{
+		{
+			numbers:  []int{1, 4, -4, 2, 5, 3, 1},
+			expected: 5,
+		},
+		{
+			numbers:  []int{1, 4, -4, 20, 5, 3, 1},
+			expected: 20,
+		},
+		{
+			numbers:  []int{1, 4, -4, 2, 1, 3, 1},
+			expected: 4,
+		},
+		{
+			numbers:  []int{1, 4, -4, 2, 1, 3, 1, 7, 8, 2},
+			expected: 8,
+		},
+	}
 
 	// Act
-	result := helper.Max(numbers)
+	for _, testCase := range testTable {
+		result := helper.Max(testCase.numbers)
+		expected := testCase.expected
 
-	// Assert
-	if result != expected {
-		t.Errorf("Incorrect result. Expect %d, got %d", expected, result)
+		t.Logf("Calling Max(%v), result %d\n", testCase.numbers, result)
+
+		// Assert
+		assert.EqualValuesf(t, testCase.expected, result,
+			fmt.Sprintf("Incorrect result. Expect %d, got %d", expected, result))
 	}
 }
 
 func TestMaxIndex(t *testing.T) {
 	// Arrange
-	numbers := []int{1, 4, -4, 2, 5, 3, 1}
-	expected := 4
+	testTable := []struct {
+		numbers  []int
+		expected int
+	}{
+		{
+			numbers:  []int{1, 4, -4, 2, 5, 3, 1},
+			expected: 4,
+		},
+		{
+			numbers:  []int{1, 4, -4, 20, 5, 3, 1},
+			expected: 3,
+		},
+		{
+			numbers:  []int{1, 4, -4, 2, 1, 3, 1},
+			expected: 1,
+		},
+		{
+			numbers:  []int{1, 4, -4, 2, 1, 3, 1, 7, 8, 2},
+			expected: 8,
+		},
+	}
 
 	// Act
-	result := helper.MaxIndex(numbers)
+	for _, testCase := range testTable {
+		result := helper.MaxIndex(testCase.numbers)
+		expected := testCase.expected
 
-	// Assert
-	if result != expected {
-		t.Errorf("Incorrect result. Expect %d, got %d", expected, result)
+		t.Logf("Calling Max(%v), result %d\n", testCase.numbers, result)
+
+		// Assert
+		if result != expected {
+			t.Errorf("Incorrect result. Expect %d, got %d", expected, result)
+		}
 	}
 }
